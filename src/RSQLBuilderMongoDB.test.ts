118994d2933d83e8960c92a6b4ef1f0ad9826a02
Stefan Walter <woigl@mac.com>
2025-02-17 11:05:34 +0400

    Fix/bump up packages (#2)
    * change: import of unti tests to index.ts file
    
    * fix: bump up packages
    
    * change: verison changed to 1.1.2

--- src/RSQLBuilderMongoDB.test.ts
+++ src/RSQLBuilderMongoDB.test.ts
@@ -1,13 +1,13 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect } from 'vitest';
 
-import RSQLBuilderMongoDB from "./RSQLBuilderMongoDB";
+import { RSQLBuilderMongoDB } from './';
 
-describe("RSQLBuilderMongoDB", () => {
-  it("Test operator Regex ('=regex=')", () => {
-    expect(new RSQLBuilderMongoDB().regex("name", "Filip").toString()).toBe('name=regex="Filip"');
-  });
+describe('RSQLBuilderMongoDB', () => {
+    it("Test operator Regex ('=regex=')", () => {
+        expect(new RSQLBuilderMongoDB().regex('name', 'Filip').toString()).toBe('name=regex="Filip"');
+    });
 
-  it("Test operator Not Regex ('=notregex=')", () => {
-    expect(new RSQLBuilderMongoDB().notRegex("name", "Filip").toString()).toBe('name=notregex="Filip"');
-  });
+    it("Test operator Not Regex ('=notregex=')", () => {
+        expect(new RSQLBuilderMongoDB().notRegex('name', 'Filip').toString()).toBe('name=notregex="Filip"');
+    });
 });
