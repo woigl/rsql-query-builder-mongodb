50b3c2ed791119ab17690e7fc2a93098933131ca
Stefan Walter <woigl@mac.com>
2025-02-18 22:29:57 +0400

    add: missing unit tests

--- src/RSQLBuilderMongoDB.test.ts
+++ src/RSQLBuilderMongoDB.test.ts
@@ -3,11 +3,27 @@ import { describe, it, expect } from 'vitest';
 import { RSQLBuilderMongoDB } from './';
 
 describe('RSQLBuilderMongoDB', () => {
-    it("Test operator Regex ('=regex=')", () => {
+    it("Test operator REGEX ('=regex=')", () => {
         expect(new RSQLBuilderMongoDB().regex('name', 'Filip').toString()).toBe('name=regex="Filip"');
     });
 
-    it("Test operator Not Regex ('=notregex=')", () => {
+    it("Test operator NOT REGEX ('=notregex=')", () => {
         expect(new RSQLBuilderMongoDB().notRegex('name', 'Filip').toString()).toBe('name=notregex="Filip"');
+    });
+
+    it("Test operator LIKE ('=regex=')", () => {
+        expect(new RSQLBuilderMongoDB().like('name', 'Fil*').toString()).toBe('name=regex="Fil*"');
+    });
+
+    it("Test operator LIKE ('=notregex=')", () => {
+        expect(new RSQLBuilderMongoDB().notLike('name', 'Fil*').toString()).toBe('name=notregex="Fil*"');
+    });
+
+    it("Test operator EXISTS ('=exists=')", () => {
+        expect(new RSQLBuilderMongoDB().exists('name').toString()).toBe('name=exists=true');
+    });
+
+    it("Test operator NOT EXISTS ('=exists=')", () => {
+        expect(new RSQLBuilderMongoDB().notExists('name').toString()).toBe('name=exists=false');
     });
 });
